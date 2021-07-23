d3bc2036c48a05a3e31a3f53276a1d167ba452e0
abdullakn <abdudebanjazz@gmail.com>
2021-07-23 14:06:07 +0530

    company personal info model completed

--- companies/models.py
+++ companies/models.py
@@ -4,4 +4,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
-
+class CompanyProfile(models.Model):
+    company_name=models.CharField(max_length=200,null=True,blank=True)
+    email=models.EmailField(max_length=200,null=True,blank=True)
+    phone_number=PhoneNumberField()
+    website=models.URLField(max_length=200,null=True,blank=True)
+    descriptiion=models.CharField(max_length=200,null=True,blank=True)
